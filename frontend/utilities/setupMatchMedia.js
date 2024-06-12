248ed0a7dee53c6752704e2828257a01ff4bd9c1
“dlmartins” <martins.diegoluis@gmail.com>
2024-06-12 18:24:56 -0300

    final adjusts to keep the aplication working

--- frontend/utilities/setupMatchMedia.js
+++ frontend/utilities/setupMatchMedia.js
@@ -1,4 +1,5 @@
-if (!window.matchMedia) {
+// utilities/setupMatchMedia.js
+if (typeof window !== 'undefined' && !window.matchMedia) {
     window.matchMedia = function matchMedia(mediaQuery) {
         return {
             matches: false,
